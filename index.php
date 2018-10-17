3b2631827843b6605434f0bb8f6a20f89e8de76f
kornellewy <korneliuszlewczuk@gmail.com>
2018-10-17 19:06:41 +0200

    kjn

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset = "utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Osadnicy</title>
+</head>
+
+<body>
+    <form action="zaloguj.php" method="post">
+        login: <br><input type="text" name = "login"><br/>
+        haslo: <br><input type="password" name = "haslo"><br/>
+        <input type="submit" value="zaloguj sie">
+    </form>
+</body>
+</html>
---
+++ zaloguj.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset = "utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Osadnicy - konto</title>
+</head>
+
+<body>
+    <?php
+        $login = $_POST['login'];
+        $haslo = $_POST['haslo'];
+        echo $login;
+        echo $haslo;
+    ?>
+</body>
+</html>
